a924a7116a72dd338e7af0ff6fc7f6ee3005cb63
Orlando <79945253+Orlandopenis@users.noreply.github.com>
2021-03-19 18:54:13 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,21 +9,6 @@ CFonts.say('ALBEDO BOT', {
   align: 'center',
   gradient: ['#00ffff', 'white']
 })
-CFonts.say(`${package.name}`, {
-  font: 'console',
-  align: 'center',
-  gradient: ['red', 'magenta']
-})
-function start(file) {
-  let args = [path.join(file), ...process.argv.slice(2)]
-  CFonts.say([process.argv[0], ...args].join(' '), {
-    font: 'console',
-    align: 'center',
-    gradient: ['red', 'magenta']
-  })
-  let p = spawn(process.argv[0], args, {
-    stdio: ['inherit', 'inherit', 'inherit', 'ipc']
-  })
   .on('message', data => {
     console.log('[RECEIVED]', data)
     switch (data) {
