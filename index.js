46f2b8d7a3b16a576b444fc109b0c657b13f19ce
Orlando <79945253+Orlandopenis@users.noreply.github.com>
2021-03-19 19:04:26 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
 CFonts.say('ALBEDO BOT', {
-  font: 'pallet',
+  font: 'block',
   align: 'center',
   gradient: ['red', 'magenta']
 })
