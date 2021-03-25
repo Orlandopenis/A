0397fb056e11461f18edbfac1bb27ee6d73eae44
Orlando <79945253+Orlandopenis@users.noreply.github.com>
2021-03-25 02:55:30 -0300

    Add files via upload

--- denz.js
+++ denz.js
@@ -118,7 +118,7 @@ async function starts() {
 				} catch {
 					ppimg = 'https://i0.wp.com/www.gambarunik.id/wp-content/uploads/2019/06/Top-Gambar-Foto-Profil-Kosong-Lucu-Tergokil-.jpg'
 				}
-				teks = `╔═━──━▒ *_BOAS VINDAS_*\n╠≽️ *_Olá!_* @${num.split('@')[0]}\n╠≽️ *_Seja muito bem vindo_*\n╠≽️ *_Leia a descrição hehe!_*\n╠≽️ *_Mantenha uma conduta saúdavel!_*\n╠≽️ *_Prefix do Bot:_* #\n╚═━──━▒ *_BOAS VINDAS_*`
+				teks = `╔═━──━▒ *_BOAS VINDAS_*\n╠≽️ *_Olá!_* @${num.split('@')[0]}\n╠≽️ *_Seja muito bem vindo_*\n╠≽️ *_Leia a descrição hehe!_*\n╠≽️ *_Tenha uma boa conduta!_*\n╠≽️ *_Prefix do Bot:_* #\n╚═━──━▒ *_BOAS VINDAS_*`
 				let buff = await getBuffer(ppimg)
 				denz.sendMessage(mdata.id, buff, MessageType.image, {caption: teks, contextInfo: {"mentionedJid": [num]}})
 				} else if (anu.action == 'promote') {
